f7af3d766f6b1fbaa6deec7f3893cbe1256a0d9e
Aaron Mendoza <aaronmendoza@Aarons-iMac.local>
2021-10-11 02:54:38 -0600

    initial build and all npms added

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "semantic-ui-css/semantic.min.css";
+import "./App.css";
+
+import ResponsiveContainer from "./components/ResponsiveContainer/ResponsiveContainer";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  useRouteMatch,
+  useParams,
+} from "react-router-dom";
+import Home from "./components/Home/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Home />
+    </Router>
   );
 }
 
---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,41 @@
+import React from "react";
+import { Container, Image, Transition } from "semantic-ui-react";
+import ResponsiveContainer from "../ResponsiveContainer/ResponsiveContainer";
+import banner from "../../Images/Banner.jpg";
+
+function Home() {
+  return (
+    <ResponsiveContainer>
+      <Image src={banner} fluid />
+      <Container fluid class="column row">
+        <iframe
+          width="100%"
+          height="300"
+          scrolling="no"
+          frameborder="no"
+          allow="autoplay"
+          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/285003278&color=%23000aff&auto_play=true&hide_related=true&show_comments=true&show_user=true&show_reposts=false&show_teaser=false&visual=true"
+        ></iframe>
+        <div>
+          <a
+            href="https://soundcloud.com/melodyinheart"
+            title="Melody In Heart"
+            target="_blank"
+          >
+            Melody In Heart
+          </a>{" "}
+          ·{" "}
+          <a
+            href="https://soundcloud.com/melodyinheart/sets/the-sippin-sessions-1"
+            title="The Sippin Sessions."
+            target="_blank"
+          >
+            The Sippin Sessions.
+          </a>
+        </div>
+      </Container>
+    </ResponsiveContainer>
+  );
+}
+
+export default Home;
